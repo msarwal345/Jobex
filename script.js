e297086fad0d659da1513af78fc3782a2b226b43
msarwal345 <142295467+msarwal345@users.noreply.github.com>
2023-09-02 23:39:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+// const cardData = [
+//     {
+//       title: "Reverse-Engineering",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i1.png"
+//     },
+//     {
+//       title: "Web",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i5.jpeg"
+//     },
+//     {
+//       title: "PWN",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i1.png"
+//     },
+//     {
+//       title: "Fundamentals",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i5.jpeg"
+//     },
+//     {
+//       title: "Intro to Cybersecurity",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i1.png"
+//     },
+//     {
+//       title: "Program Security",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i5.jpeg"
+//     },
+//     {
+//       title: "System Security",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i1.png"
+//     },
+//     {
+//       title: "Software Exploitation",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i5.jpeg"
+//     },
+//     {
+//       title: "Archived Modules",
+//       body: "Reverse engineering is a meticulous and strategic process used to unravel the intricacies of technology, software, or products. Through this methodical approach, experts deconstruct and analyze these systems to uncover their underlying architecture, logic, and functionality. Reverse engineering serves as a bridge between the observable behavior of a system and the intricate code that powers it. It involves processes such as disassembly, code analysis, and data flow mapping to comprehend how a product or software operates. This practice finds applications across various domains, from software development to security assessment. In the realm of software, reverse engineering helps developers understand legacy systems, enabling them to modify, improve, or integrate new functionalities. In cybersecurity, it aids in identifying vulnerabilities and potential exploits by examining the inner workings of software.However, reverse engineering should be conducted ethically and legally, respecting intellectual property rights and regulations. It empowers researchers and engineers to learn from existing technology, refine their skills, and contribute to innovation by building upon the knowledge hidden within complex systems.",
+//       image: "Resources/i1.png"
+//     }
+// ];
+// var index=8;
+// document.querySelectorAll('a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// function handle(index,obj){
+//     var cardD=cardData[index];
+//     localStorage.setItem('selectedItem',JSON.stringify(cardD));
+//     window.location.href = 'template_blog.html';
+// }
+// var card=document.querySelectorAll('#cards');
+// card.forEach((card,idx) => {
+//     card.addEventListener('click',()=>{
+//         handle(idx);
+//         windows.href="hi.html";
+//     });
+// });
+
+
+// function form_sub(){
+//   obj ={
+//     title:document.getElementById('ft'),
+//     auth:document.getElementById('fa'),
+//     cat:document.getElementById('fc'),
+//     image:document.getElementById('fi'),
+//     para:document.getElementById('ftext'),
+//   };
+
+//   // localStorage.setItem(nblog,JSON.stringify(obj))
+//   index++;
+//   document.getElementById('#cards').innerHTML+='<div class="card"><h1>SOFTWARE DEVLOPER</h1><p>1234 jobs available</p><a id="'+index+' card"  href="hi.html">Know More</a></div>'
+//   cardData.push(obj);
+// }
+
+
+document.addEventListener("DOMContentLoaded",function() {
+    const goToTop=document.getElementById("gottotop");
+    window.onscroll=function(){
+    if(document.body.scrollTop > 20 ||document.documentElement.scrollTop>20)
+        goToTop.style.display="block";
+    else goToTop.style.display="none";
+    }
+
+gototop.addEventListener("click",function() {
+    document.documentElement.scrollTop=0;
+    document.body.scrollTop=0;
+});
+});
+
+
+// const cards= document.querySelectorAll('#card');
+// cards.forEach((cards,idx)=>{
+//     cards.addEventListener('click',()=>{
+//         handle(idx);
+//     });
+// });
+
+// function handle(idx){
+//     localStorage.setItem(x,)
+// }
